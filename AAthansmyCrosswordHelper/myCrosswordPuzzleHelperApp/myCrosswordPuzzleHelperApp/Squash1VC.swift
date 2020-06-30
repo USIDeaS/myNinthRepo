4050f13b8da8527e88e8f478f82aef0e4e56c7c6
Anastasia Athans <64998016+USIDeaS@users.noreply.github.com>
2020-06-29 22:30:28 -0500

    Add files via upload

---
+++ AAthansmyCrosswordHelper/myCrosswordPuzzleHelperApp/myCrosswordPuzzleHelperApp/Squash1VC.swift
@@ -0,0 +1,30 @@
+//
+//  Squash1VC.swift
+//  myCrosswordPuzzleHelperApp
+//
+//  Created by Anastasia Athans-Stothoff on 6/29/20.
+//  Copyright © 2020 Anastasia Athans-Stothoff. All rights reserved.
+//
+
+import UIKit
+
+class Squash1VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AAthansmyCrosswordHelper/myCrosswordPuzzleHelperApp/myCrosswordPuzzleHelperApp/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  myCrosswordPuzzleHelperApp
+//
+//  Created by Anastasia Athans-Stothoff on 6/29/20.
+//  Copyright © 2020 Anastasia Athans-Stothoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func bark1(_ sender: Any) {
+       
+    }
+    
+    @IBAction func bark2(_ sender: Any) {
+       
+    }
+
+    @IBAction func hatch1(_ sender: Any) {
+       
+    }
+    
+    @IBAction func hatch2(_ sender: Any) {
+       
+    }
+    
+    @IBAction func squash1(_ sender: Any) {
+        
+    }
+    
+    @IBAction func squash2(_ sender: Any) {
+       
+    }
+    
+
+
+}
+
